e0f77337a24ecc060bba4627755b08c7eec620b6
Christian Canonigo <28989015+cjcanonigo@users.noreply.github.com>
2018-09-11 13:03:09 -0700

    added read file example.

---
+++ 04server.js
@@ -0,0 +1,21 @@
+//Edit the file system
+let http = require('http');
+let fs = require('fs');
+let htmlFile = '04html.html';
+
+let pageData = (req, res) => {
+    fs.readFile(htmlFile, (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data);
+        res.end();
+    });
+};
+
+http.createServer(pageData).listen(8080);
+
+let stringLiteral = `
+server04
+File System manipulation - read a file.
+`;
+
+console.log(stringLiteral);
